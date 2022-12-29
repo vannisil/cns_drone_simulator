32188f56f34637ade8c166892ca535724e0eec37
Alessandro Iannone <iann270599@gmail.com>
2022-12-29 10:45:07 +0100

    Fix delete

--- client/src/components/Cu.js
+++ client/src/components/Cu.js
@@ -2,13 +2,13 @@ import React from 'react'
 import {BiEdit} from "react-icons/bi"
 import {AiFillDelete} from "react-icons/ai"
 
-const Cu = ({text, updateMode, deletePath}) => {
+const Cu = ({text, updateMode, deleteCu}) => {
     return (
         <div className="cu">
             <div className="text">{text}</div>
             <div className="icons">
                 <BiEdit className='icon' onClick={updateMode} />
-                <AiFillDelete className='icon' onClick={deletePath} />
+                <AiFillDelete className='icon' onClick={deleteCu} />
             </div>
         </div>
     )
