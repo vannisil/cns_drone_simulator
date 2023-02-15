e92fd90f2a8ccf57e05dd44919068da3fd30a475
Alessandro Iannone <iann270599@gmail.com>
2023-02-15 19:48:10 +0100

    creato servizio di simulazione

--- client/src/Simulator.js
+++ client/src/Simulator.js
@@ -1,19 +1,41 @@
 import { useEffect, useState } from "react";
-import {getAllCu} from "./utils/HandleApi";
+import { getAllCu } from "./utils/HandleApi";
 
+
+const url = 'http://localhost:5000';
 
 function Simulator() {
     const [cu, setCu] = useState([])
+    const [latitude, setLatitude] = useState("")
 
     useEffect(() => {
         getAllCu(setCu)
-      }, [])
+    }, [])
 
-    
+
+    const get = async () => {
+        fetch(url + '/getPosition')
+            .then((response) => response.json())
+            .then((position) => setLatitude(position.latitude))
+            .then((position) => console.log("latitude:" + position.latitude))
+    }
+
+    const ok = () => {
+        console.log(latitude)
+    }
+
     return (
         <div className="Home">
             <div className="centered">
-            <h1>Simulator</h1>
+                <h1>Simulator</h1>
+                <button onClick={ok}>
+                    ok
+                </button>
+            </div>
+
+            <div
+                className="addCu"
+                onClick={get}>
             </div>
         </div>
     );
--- server/app.js
+++ server/app.js
@@ -4,12 +4,19 @@ const routeCreatePath = require('./routes/createRoute')
 const cors = require("cors")
 
 require('dotenv').config()
+var obj = new Object
+obj.latitude = "41.088051597226894";
+obj.longitude = "16.839490137794275";
 
 const app = express()
 const PORT = process.env.port || 5000
 
 app.use(express.json())
 app.use(cors())
+
+app.get("/getPosition", (req, res) => {
+    res.send(obj);
+})
 
 mongoose
     .connect(process.env.MONGODB_URL)
