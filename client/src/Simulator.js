e56f0b154acd5be560ce69684bea897947e47fcf
Alessandro Iannone <iann270599@gmail.com>
2023-02-17 10:09:24 +0100

    aggiunta verifica della posizione

--- client/src/Simulator.js
+++ client/src/Simulator.js
@@ -1,14 +1,16 @@
 import { useEffect, useState } from "react";
 import PathSimulator from "./components/PathSimulator";
 import { getAllPath, addPath, updatePath, deletePath } from "./utils/HandleApi";
+import { AiFillCloseCircle } from "react-icons/ai"
 import { getAllCu } from "./utils/HandleApi";
 
 
 const url = 'http://localhost:5000';
 
 function Simulator() {
-    const [cu, setCu] = useState([])
     const [path, setPath] = useState([])
+    const [modal, setModal] = useState(false);
+    const [popText, setPopText] = useState("");
 
     useEffect(() => {
         getAllPath(setPath)
@@ -25,16 +27,17 @@ function Simulator() {
         fetch(url + '/getPosition')
             .then((response) => response.json())
             .then((position) => latitude = position.latitude)
-        // .then(() => console.log("latitude:" + latitude))
 
         fetch(url + '/getPosition')
             .then((response) => response.json())
             .then((position) => longitude = position.longitude)
-        // .then(() => console.log("longitude:" + longitude))
 
     }
 
-    var step;
+    var step = 0;
+    var isInPath;
+    var step1;
+    var step2;
 
     const get = () => {
         console.log("latitude:" + latitude + "\n longitude:" + longitude)
@@ -50,22 +53,42 @@ function Simulator() {
             var time = (obj[i].time)
             var time1 = (obj[i + 1].time)
             if (date < currentDate) {
-                if(date1 < currentDate){
-                } else if(date1 >= currentDate){
-                    if(time1 >= currentTime){
-                        var step = i + 1;
+                if (date1 < currentDate) {
+                } else if (date1 >= currentDate) {
+                    if (time1 >= currentTime) {
+                        step = i + 1;
                     }
                 }
             }
-            else if(date === currentDate){
-                if(date1 === currentDate){
-                    if(time1 >= currentTime && time < currentTime){
-                        var step = i + 1;
+            else if (date === currentDate) {
+                if (date1 === currentDate) {
+                    if (time1 >= currentTime && time < currentTime) {
+                        step = i + 1;
                     }
                 }
             }
         }
+        if (step === 0) {
+            isInPath = false;
+        }
+        var latitude1 = obj[(step - 1)].latitude;
+        var latitude2 = obj[step].latitude;
+        if (latitude1 < latitude && latitude2 >= latitude) {
+            isInPath = true;
+        } else isInPath = false;
+
+        var longitude1 = obj[step - 1].longitude;
+        var longitude2 = obj[step].longitude;
+        if (longitude1 < longitude && longitude2 >= longitude) {
+            isInPath = true;
+        } else isInPath = false;
+
+        step1 = obj[step - 1].name;
+        step2 = obj[step].name;
         console.log(step)
+        console.log(isInPath)
+        console.log(obj[step].latitude)
+        toggleModal()
     }
 
     var obj;
@@ -75,6 +98,16 @@ function Simulator() {
         obj = JSON.parse(json);
         return obj[0].namepath
     }
+
+    const toggleModal = () => {
+        var popUpText = "your vehicle is between " + step1 + " and " + step2;
+        setPopText(popUpText)
+        setModal(!modal);
+      };
+    
+      const toggleModalExit = () => {
+        setModal(!modal);
+      };
 
     return (
         <div className="Home">
@@ -94,6 +127,21 @@ function Simulator() {
             <div
                 className="addCu"
                 onClick={get}>
+            </div>
+            <div className="cuCentered">
+                {modal && (
+                    <div className="modal">
+                        <div onClick={toggleModal} className="overlay"></div>
+                        <div className="modal-content">
+                            <br></br>
+                            <p>
+                                {popText}
+                            </p>
+                            <br></br>
+                            <AiFillCloseCircle className='close-modal' onClick={toggleModalExit} />
+                        </div>
+                    </div>
+                )}
             </div>
         </div>
     );
